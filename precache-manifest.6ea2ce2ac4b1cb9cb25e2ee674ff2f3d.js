d53cd48e6e58b9de9347b6715171e717a3953461
gaev-art <forsakensr@gmail.com>
2020-05-08 22:42:38 +0300

    Updates

---
+++ precache-manifest.6ea2ce2ac4b1cb9cb25e2ee674ff2f3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43f098162b9ec6d5b6a279817fc638f",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "c97468d82fe831164497",
+    "url": "/social-network/static/css/main.7a646e59.chunk.css"
+  },
+  {
+    "revision": "22432a0cf7441b9a63dd",
+    "url": "/social-network/static/js/2.b2032da9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.b2032da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97468d82fe831164497",
+    "url": "/social-network/static/js/main.d6543e72.chunk.js"
+  },
+  {
+    "revision": "d760fa252db9374406b8",
+    "url": "/social-network/static/js/runtime-main.b9cba0c9.js"
+  },
+  {
+    "revision": "74201d1c52eda8bf21d5a3c49098419c",
+    "url": "/social-network/static/media/big.74201d1c.jpg"
+  },
+  {
+    "revision": "62af280a0ad67c8d2efee6939afd87bc",
+    "url": "/social-network/static/media/logo.62af280a.png"
+  }
+]);
